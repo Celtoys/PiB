54a551a3bc681db2a682ad3c72808e641a928cee
dwilliamson <dwilliamson@users.noreply.github.com>
2021-04-12 10:48:04 +0100

    Download progress and MSI extract support.

--- Python/Packages.py
+++ Python/Packages.py
@@ -26,6 +26,7 @@
 # Packages.py: Experimental, simple package "manager"
 #
 
+import io
 import os
 import shutil
 import tempfile
@@ -39,16 +40,47 @@ def DownloadFile(url):
     # Generate a temporary download location
     filename = os.path.join(tempfile._get_default_tempdir(), next(tempfile._get_candidate_names()))
 
-    # Download the file
-    print(f"   Downloading {url}")
-    with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
-        shutil.copyfileobj(response, out_file)
+    print(f"Downloading {url}")
+
+    # Query server for download
+    response = urllib.request.urlopen(url)
+    length = response.getheader("content-length")
+
+    # Determine download block size for progress
+    if length:
+        print(f"   Size: {length} bytes")
+        length = int(length)
+        block_size = max(4096, length // 100)
+    else:
+        print("   Size Unknown")
+        block_size = 1024 * 1024
+
+    # Download in blocks
+    buffer = io.BytesIO()
+    size = 0
+    while True:
+        block_buffer = response.read(block_size)
+        if not block_buffer:
+            break
+        buffer.write(block_buffer)
+        size += len(block_buffer)
+
+        # Display progress percentage
+        if length:
+            print(f"\t{int(size / length * 100)} %\r", end="")
+
+    # Clear progress line
+    print()
+
+    # Write download to file
+    with open(filename, "wb") as f:
+        f.write(buffer.getbuffer())
 
     return filename
 
 def ExtractZipFileTo(filename, path):
     # Extract entire zip file
-    print(f"   Extracting to {path}")
+    print(f"Extracting {filename} to {path}")
     with zipfile.ZipFile(filename) as zf:
         zf.extractall(path)
 
@@ -58,6 +90,15 @@ def ExtractZipFile(filename):
     # Generate a temporary extract location
     path = os.path.join(tempfile._get_default_tempdir(), next(tempfile._get_candidate_names()))
     return ExtractZipFileTo(filename, path)
+
+def ExtractMsiFileTo(filename, path):
+    print(f"Extracting {filename} to {path}")
+    os.system(f"msiexec /a {filename} /qb TARGETDIR={path}")
+    return path
+
+def ExtractMsiFile(filename):
+    path = os.path.join(tempfile._get_default_tempdir(), next(tempfile._get_candidate_names()))
+    return ExtractMsiFileTo(filename, path)
 
 # 7-Zip not installed by default
 SevenZipExe = None
